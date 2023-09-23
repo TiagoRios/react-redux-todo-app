82e5b8e2e8e53802888dfbdafcc53d18a59544d6
Tiago Rios <tiago_pc9@hotmail.com>
2023-09-22 21:58:51 -0300

    store do redux criada e configurada com RTK.

---
+++ starter/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore} from '@reduxjs/toolkit';
+import todoReducer from './todoSlice';
+
+// a loja guarda todos os redutores e os gerencia para nós
+export default configureStore({
+    reducer: {
+        todos: todoReducer
+    }
+})
