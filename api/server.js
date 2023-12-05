0a14e946ed460c50db35265dc8521276a95362aa
Tiago Rios <tiago_pc9@hotmail.com>
2023-12-05 16:48:34 -0300

    Refatoração - Ajustes na API.

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const dotenv = require('dotenv');
 const colors = require('colors');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+
 const { json } = require('body-parser');
 const { nanoid } = require('nanoid');
 
@@ -12,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-let todos = [
+const todos = [
 	{ id: nanoid(), title: 'dormir antes das 22h', completed: false },
 	{ id: nanoid(), title: 'alimentar cão', completed: true },
 	{ id: nanoid(), title: 'passear cão', completed: true },
@@ -25,22 +26,26 @@ app.get('/todos', (req, res) => res.send(todos));
 app.post('/todos', (req, res) => {
 	const todo = { title: req.body.title, id: nanoid(), completed: false };
 	todos.push(todo);
+
 	return res.send(todo);
 });
 
 app.patch('/todos/:id', (req, res) => {
-	const id = req.params.id;
+	const { id } = req.params;
 	const index = todos.findIndex((todo) => todo.id == id);
 	const completed = Boolean(req.body.completed);
+
 	if (index > -1) {
 		todos[index].completed = completed;
 	}
+
 	return res.send(todos[index]);
 });
 
 app.delete('/todos/:id', (req, res) => {
-	const id = req.params.id;
+	const { id } = req.params;
 	const index = todos.findIndex((todo) => todo.id == id);
+
 	if (index > -1) {
 		todos.splice(index, 1);
 	}
